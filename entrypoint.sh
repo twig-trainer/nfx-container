393ce0b97b5a4cf415b2e2169e2b74a68b2061ee
twig-trainer <106540887+twig-trainer@users.noreply.github.com>
2022-06-03 18:36:39 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,17 @@
 echo " start the script entrypoint.sh"
 memory=$(cat /proc/meminfo)
 echo "::set-output name=memory::$memory" 
+
+echo "start test1"
+check_status1=$(curl ifconfig.co)
+echo "::set-output name=check_status1::$check_status1" 
+
+echo "start test2"
+check_status2=$(ping 1.1.1.1)
+echo "::set-output name=check_status2::$check_status2" 
+
+echo "start test3"
+check_status3=$(curl ifconfig.co/country)
+echo "::set-output name=check_status3::$check_status3" 
+
 echo " end the entrypoint.sh"
