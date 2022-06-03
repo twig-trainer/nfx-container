c36f9450e447e7cbf7bd021b6e9708396efb70f8
twig-trainer <106540887+twig-trainer@users.noreply.github.com>
2022-06-03 18:51:10 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,7 @@ check_status1=$(curl ifconfig.co)
 echo "::set-output name=check_status1::$check_status1" 
 
 echo "start test2"
-check_status2=$(ping 1.1.1.1)
+check_status2=$(curl ifconfig.co/country)
 echo "::set-output name=check_status2::$check_status2" 
-
-echo "start test3"
-check_status3=$(curl ifconfig.co/country)
-echo "::set-output name=check_status3::$check_status3" 
 
 echo " end the entrypoint.sh"
